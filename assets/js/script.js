bcecec9666b1f8ca8929e8e7386887a357f7933f
Marcin Tatarski <martin.tatarski>
2023-12-09 22:33:08 +0000

    Add switch between tabs/dates. Add save to local storage each day

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
   }
   if(!getLocalStorage) getLocalStorage = [];
   
-  console.log(getLocalStorage);
+  // console.log(getLocalStorage);
+
    // Current day
-  const currentDay = dayjs();
-  const workDay = currentDay.format('dddd, MMMM DD')
+  // const currentDay = dayjs();
+  const workDay = dayjs().format('dddd, MMMM DD')
 
   // Display current day
   $('#currentDay').text(workDay);
@@ -17,9 +18,37 @@ $(document).ready(function() {
   const currentHour = Number(dayjs().format('H'));
   // console.log(dayjs().daysInMonth());
 
+  // Days of the month
+  const daysInTheMonth = dayjs().daysInMonth();
+  const dayOfTheMonth = Number(dayjs().format('D'));
+
+  const arr = Array.from({length: daysInTheMonth}, (_, i)=> i + 1);
+  const ul = $('#nav');
+  ul.attr('data-day', dayOfTheMonth);
+  
+  arr.forEach((day)=>{
+    const li = $('<li>');
+    li.addClass('nav-item');
+    
+    const a = $('<button>')
+    a.addClass('nav-link');
+
+    if(day === dayOfTheMonth){
+      a.addClass('active');
+      a.attr('aria-current', 'page');      
+    }
+
+    a.text(day);
+
+    li.append(a);
+    ul.append(li);    
+  })
+
+
   $('.hour').each(function () {
     const hour = Number($(this).attr('data-hour'));
-    
+    // const dataText = $(this).next().find('textarea').val();
+   
     if(hour === currentHour) {
       $(this).parent().addClass('present');
     }else if(hour > currentHour){
@@ -32,16 +61,91 @@ $(document).ready(function() {
   })
 
 
-  // Save to local storage
-  $('#save').on('click', 'button' , function(e) {    
-    // console.log(e);
-   
-    let findCurrentSchedule = getLocalStorage.find((data) => data.date === dayjs().format('dddd, MMMM DD'));
+  let findCurrentSchedule = getLocalStorage.find((data) => data.date === dayjs().format('dddd, MMMM DD'));
 
-    // console.log(findCurrentSchedule);
+  // Populate with data for current work schedule  
+  if(findCurrentSchedule){   
+    // console.log(findCurrentSchedule.data[9]);
+    $('.hour').each(function(){
+      const hour = $(this).attr('data-hour');
+      const dataText = $(this).next().find('textarea');
+
+      dataText.text(findCurrentSchedule.data[hour]); 
+    })
+  }
+
+
+  // Change the date
+  $('#nav').on('click', '.nav-item' ,function(){
+
+    // Change the tab day 
+    const day = $(this).children();   
+   
+    // Remove previous active tab
+    const links = $('.nav-link');
+    for(let i = 0; i < links.length; i++){
+      const link = $(links[i]);
+            
+      if(link.hasClass('active')){
+        link.removeClass('active');
+        link.removeAttr('aria-current');
+        break;
+      }
+    }
+    
+    // Set new active tab
+    day.attr('aria-current', 'page');
+    day.addClass('active');
+
+    // Set new date
+    const newDate = dayjs().set('date', day.text()).format('dddd, MMMM DD');  
+    
+        // Find data based of new date
+    let searchSchedule = getLocalStorage.find((data) => data.date === newDate);
+ 
+
+    if(!searchSchedule) {
+      $('.hour').each(function(){
+        const dataText = $(this).next().find('textarea');         
+        dataText.text(''); 
+      })
+    }else{
+      $('.hour').each(function(){
+        const hour = $(this).attr('data-hour');
+        const dataText = $(this).next().find('textarea');
+  
+        dataText.text(searchSchedule.data[hour]); 
+      })
+    }
+
+  })  
+
+
+
+
+  // Save to local storage
+  $('#save').on('click', 'button' , function() {    
+    
+    const links = $('.nav-link');
+    let day = '';
+    for(let i = 0; i < links.length; i++){
+      const link = $(links[i]);
+            
+      if(link.hasClass('active')){
+        day = link.text();
+        break;
+      }
+    }
+
+    // Set new date
+    const newDate = dayjs().set('date', day).format('dddd, MMMM DD');  
+
+    let findCurrentSchedule = getLocalStorage.find((data) => data.date === newDate);
+   
+
     if(!findCurrentSchedule){
       const saveTodayWorkSchedule = {
-        date: dayjs().format('dddd, MMMM DD'),
+        date: newDate,
         data: {}
       };
 
@@ -71,11 +175,4 @@ $(document).ready(function() {
   });
 
   
-
-
-
-
-
-
-
 });
